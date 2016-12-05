b66699fb1b6c3b725217ce2289335145fc7ec489
Mark Brazinski <markbrazinski@gmail.com>
2016-12-05 23:58:02 +0000

    added copy to example

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require("react");
 var Examples = React.createClass({
     render: function (){
         return(
-            <h2>Examples Page</h2>    
+            <div>
+                <h2>Examples Page</h2>
+                <p>examples page </p>
+            </div>
         );
     }
 });
