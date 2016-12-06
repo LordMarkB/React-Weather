9e0140f4aff66102370beddf89df85ddbdfb770c
Mark Brazinski <markbrazinski@gmail.com>
2016-12-06 01:07:37 +0000

    actually fixed the server this time

--- server.js
+++ server.js
@@ -1,14 +1,25 @@
-var express = require("express");
+var   express = require("express");
+var   app     = express();
+const Port    = (process.env.PORT || 3000);
 
 
-var app = express();
-
-app.use(express.static(__dirname + "/public"))
-
-app.get("/", function(req,res){
-    res.render("index")
-})
-
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("Bring me to your leader");
+//another thing
+app.use(function(req,res,next){
+    if(req.headers['x-forwarded-proto']==='http'){
+        next();
+    }else {
+        res.redirect("http://"+req.hostname+req.url);
+    }
 });
+app.use(express.static(__dirname + "/public"));
+// app.get("/", function(req,res){
+//     res.render("index");
+// });
+
+
+app.listen(Port, process.env.IP, function(){
+   console.log("Bring me to your leader ");
+});
+
+
+console.log('using the updated server');
